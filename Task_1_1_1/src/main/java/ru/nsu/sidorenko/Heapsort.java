8282b27e2a8c5ea5cfe27d64c0ef7ffcd2b2dc39
Черемушкин Евгений Сергеевич <e.cheremushkin@main.nsu.ru>
2025-09-22 09:15:42 +0700

    review fix

--- Task_1_1_1/src/main/java/ru/nsu/sidorenko/Heapsort.java
+++ Task_1_1_1/src/main/java/ru/nsu/sidorenko/Heapsort.java
@@ -6,31 +6,6 @@ package ru.nsu.sidorenko;
 public final class Heapsort {
 
     /**
-     * Делает из массива двоичное дерево, в котором каждый родитель больше своих сыновей.
-     *
-     * @param arr - массив, который будет обработан
-     * @param n - длина обрабатываемого массива
-     * @param i - родитель, который обрабатывается в данный момент.
-     */
-
-    public static void heapify(int[] arr, int n, int i) {
-        int maxid = i;
-        int l = i * 2 + 1;
-        int r = i * 2 + 2;
-        if (l < n && arr[l] > arr[maxid]) {
-            maxid = l;
-        }
-        if (r < n && arr[r] > arr[maxid]) {
-            maxid = r;
-        }
-
-        if (maxid != i) {
-            swap(arr, i, maxid);
-            heapify(arr, n, maxid);
-        }
-    }
-
-    /**
      * Реализация пирамидальной сортировки по возрастанию.
      *
      * @param arr - сортируемый массив
@@ -48,6 +23,30 @@ public final class Heapsort {
         }
     }
 
+    /**
+     * Делает из массива двоичное дерево, в котором каждый родитель больше своих сыновей.
+     *
+     * @param arr - массив, который будет обработан
+     * @param n - длина обрабатываемого массива
+     * @param i - родитель, который обрабатывается в данный момент.
+     */
+
+    private static void heapify(int[] arr, int n, int i) {
+        int maxid = i;
+        int l = i * 2 + 1;
+        int r = i * 2 + 2;
+        if (l < n && arr[l] > arr[maxid]) {
+            maxid = l;
+        }
+        if (r < n && arr[r] > arr[maxid]) {
+            maxid = r;
+        }
+
+        if (maxid != i) {
+            swap(arr, i, maxid);
+            heapify(arr, n, maxid);
+        }
+    }
 
     /**
      * Меняет местами два элемента из массива arr, где эти элементы имеют индексы i и j.
