ca69e14db1f55e680290e6b7cbecb05a8d3b7420
alassskaa <alassskaa@users.noreply.github.com>
2025-12-05 05:59:01 +0000

    Deploying to gh-pages from @ alassskaa/OOP@f9353576dacfd7343f96a7da39f83dcefa86e081 🚀

--- Task_1_3_1/type-search-index.js
+++ Task_1_3_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.sidorenko","l":"FindString"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.nsu.sidorenko","l":"StringFinder"}];updateSearchResults();
